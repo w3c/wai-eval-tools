0507540cfb2009c0aa8f8b443b8889d0b58c6bcb
Eric Eggert <mail@yatil.net>
2014-12-11 18:27:24 +0100

    Have configuration in a config.php

---
+++ config.php
@@ -0,0 +1,212 @@
+<?php
+
+$language = array(
+	title => "Tool languages:",
+	data => array(
+		"pt-br" => 'Brazilian Portuguese (<span lang="pt-br">Português Brasileiro</span>)',
+		"bg" => 'Bulgarian (<span lang="bg">Български</span>)',
+		"zh-hans" => 'Chinese, Simplified (<span lang="zh-hans">简体中文</span>)',
+		"zh-hant" => 'Chinese, Traditional (<span lang="zh-hant">繁體中文</span>)',
+		"cs" => 'Czech (<span lang="cs">Česky</span>)',
+		"da" => 'Danish (<span lang="da">Dansk</span>)',
+		"nl" => 'Dutch (<span lang="nl">Nederlands</span>)',
+		"en" => 'English',
+		"fi" => 'Finnish (<span lang="fi">Suomi</span>)',
+		"fr" => 'French (<span lang="fr">Français</span>)',
+		"gl" => 'Galician (<span lang="gl">Galego</span>)',
+		"de" => 'German (<span lang="de">Deutsch</span>)',
+		"it" => 'Italian (<span lang="it">Italiano</span>)',
+		"ja" => 'Japanese (<span lang="ja">日本語</span>)',
+		"ko" => 'Korean (<span lang="ko">한국어</span>)',
+		"pl" => 'Polish (<span lang="pl">Polski</span>)',
+		"pt" => 'Portuguese (<span lang="pt">Português</span>)',
+		"ro" => 'Romanian (<span lang="ro">Română</span>)',
+		"sr" => 'Serbian (<span lang="sr">Српски</span>)',
+		"es" => 'Spanish (<span lang="es">Castellano</span>)'
+	),
+	other => true,
+	variable => 'language',
+	type => 'checkbox'
+);
+
+$guideline = array(
+	title => "Select Guidelines that can be checked using this tool:",
+	data => array(
+		"wcag20" => '<strong><abbr title="Web Content Accessibility Guidelines">WCAG</abbr> 2.0 — <abbr title="World Wide Web Consortium">W3C</abbr> Web Content Accessibility Guidelines 2.0</strong>',
+		"wcag10" => '<abbr title="Web Content Accessibility Guidelines">WCAG</abbr> 1.0 — <abbr title="World Wide Web Consortium">W3C</abbr> Web Content Accessibility Guidelines 1.0',
+		"section508" => 'Section 508, <abbr title="United States">US</abbr> federal procurement standard',
+		"jis" => '<abbr title="Japanese Industry Standard">JIS</abbr>, Japanese industry standard',
+		"stanca" => 'Stanca Act, Italian accessibility legislation',
+		"bitv20" => '<abbr lang="de" title="Barrierefreie Informationstechnik-Verordnung 2.0">BITV 2.0</abbr>, German government standard',
+		"bitv10" => '<abbr lang="de" title="Barrierefreie Informationstechnik-Verordnung">BITV</abbr>, German government standard',
+		"rgaa" => '<abbr lang="fr" title="Référentiel Général d’Accessibilité pour les Administrations">RGAA</abbr>, French government standard'
+	),
+	other => true,
+	variable => 'guideline',
+	type => 'checkbox'
+);
+
+$assists = array(
+	title => "Assists evaluations by:",
+	data => array(
+		"report" => 'Generating reports of evaluation results',
+		"wizard" => 'Providing step-by-step evaluation guidance',
+		"inpage" => 'Displaying information within web pages',
+		"transformation" => 'Modifying the presentation of web pages'
+	),
+	other => false,
+	variable => 'assists',
+	type => 'checkbox',
+	explanation => array(
+		"report" => 'http://www.w3.org/WAI/eval/selectingtools.html#reports',
+		"wizard" => 'http://www.w3.org/WAI/eval/selectingtools.html#wizards',
+		"inpage" => 'http://www.w3.org/WAI/eval/selectingtools.html#in-page',
+		"transformation" => 'http://www.w3.org/WAI/eval/selectingtools.html#transformation'
+	)
+);
+
+$automated = array(
+	title => "Supports automated checking of:",
+	data => array(
+		"automated" => 'Single web pages',
+		"crawl" => 'Groups of web pages or web sites',
+		"authenticated" => 'Restricted or password protected pages'
+	),
+	other => false,
+	variable => 'automated',
+	type => 'checkbox'
+);
+
+$repair = array(
+	title => "Provides these repair options:",
+	data => array(
+		"repair" => 'Changes the code of the web pages',
+		"caption" => 'Helps caption audio or video content',
+		"pdf2html" => 'Converts <abbr title="Portable Document Format">PDF</abbr> files into accessible <abbr title="Hypertext Markup Language">HTML</abbr>',
+		"word2html" => 'Converts Word files into accessible <abbr title="Hypertext Markup Language">HTML</abbr>',
+		"excel2html" => 'Converts Excel files into accessible <abbr title="Hypertext Markup Language">HTML</abbr>',
+		"ppt2html" => 'Converts PowerPoint files into accessible <abbr title="Hypertext Markup Language">HTML</abbr>'
+	),
+	other => true,
+	variable => 'repair',
+	type => 'checkbox'
+);
+
+$technology = array(
+	title => "Checks the accessibility of:",
+	data => array(
+		"css" => '<abbr title="Cascading Style Sheets">CSS</abbr>',
+		"html" => '<abbr title="Hypertext Markup Language">HTML</abbr>',
+		"xhtml" => '<abbr title="Extensible Hypertext Markup Language">XHTML</abbr>',
+		"svg" => '<abbr title="Scalabale Vector Grpahics">SVG</abbr>',
+		"pdf" => '<abbr title="Portable Document Format">PDF</abbr> documents',
+		"images" => 'Images',
+		"smil" => '<abbr title="Synchronized Multimedia Integration Language">SMIL</abbr>',
+		"office" => 'Microsoft Office documents'
+	),
+	other => true,
+	variable => 'technology',
+	type => 'checkbox'
+);
+
+$onlineservice = array(
+	title => "Online service:",
+	data => array(
+		"online" => 'Online checker',
+		"hosted" => 'Hosted service',
+		"server" => 'Server installation'
+	),
+	other => false,
+	variable => 'onlineservice',
+	type => 'checkbox'
+);
+
+$desktopapp = array(
+	title => "Desktop application for:",
+	data => array(
+		"windows" => 'Microsoft Windows',
+		"osx" => 'Apple (Mac) OS X',
+		"linux" => 'Linux',
+		"solaris" => 'Solaris',
+		"bsd" => '<abbr title="Berkley Shell Distribution">BSD</abbr> Unix'
+	),
+	other => true,
+	variable => 'desktop',
+	type => 'checkbox'
+);
+
+$mobileapp = array(
+	title => "Mobile application for:",
+	data => array(
+		"ios" => 'Apple iOS',
+		"android" => 'Android',
+		"windows" => 'Microsoft Windows Phone',
+		"blackberry" => 'Blackberry',
+		"firefox" => 'Firefox OS'
+	),
+	other => true,
+	variable => 'mobile',
+	type => 'checkbox'
+);
+
+$authoringtools = array(
+	title => "Authoring tool plugin for:",
+	data => array(
+		"dreamweaver" => 'Adobe Dreamweaver',
+		"flash" => 'Adobe Flash',
+		"photoshop" => 'Adobe Photoshop',
+		"edge" => 'Adobe Edge',
+		"eclipse" => "Eclipse",
+		"expressionweb" => 'Microsoft Expression Web',
+		"visualstudio" => 'Microsoft Visual Studio',
+		"sublimetext" => 'Sublime Text'
+	),
+	other => true,
+	variable => 'authoringtools',
+	type => 'checkbox'
+);
+
+$browsers = array(
+	title => "Browser plugin for:",
+	data => array(
+		"chrome" => 'Chrome',
+		"firefox" => 'Firefox',
+		"oldie" => 'Internet Explorer &le; 8',
+		"ie" => 'Internet Explorer &ge; 9',
+		"oldopera" => 'Opera &le; 12',
+		"opera" => 'Opera &ge; 15',
+		"safari" => 'Safari'
+	),
+	other => true,
+	variable => 'browsers',
+	type => 'checkbox'
+);
+
+$license = array(
+	title => 'License type:',
+	data => array(
+		"free" => 'Free Software',
+		"open" => 'Open Source',
+		"trial" => 'Trial or Demo',
+		"commercial" => 'Commercial',
+		"enterprise" => 'Enterprise'
+	),
+	other => false,
+	variable => 'license',
+	type => 'checkbox'
+);
+
+$tooltype = array(
+	title => 'Type of tool:',
+	data => array(
+		"desktop" => 'Desktop application',
+		"mobile" => 'Mobile application',
+		"cli" => 'Command line tool',
+		"browserplugin" => 'Browser plugin',
+		"atplugin" => 'Authoring tool plugin',
+		"online" => 'Online tool'
+	),
+	other => false,
+	variable => 'tooltype',
+	type => 'checkbox'
+);
